bc193beefffce2de91eac0dba2565007bc7318c1
artsoroka <githubrepository@yandex.ru>
2015-05-04 11:27:56 -0400

    worker gets message containing form data and meta fields and then composes html email

--- worker.js
+++ worker.js
@@ -1,17 +1,23 @@
 var context = require('rabbit.js').createContext();
 var mailer  = require('./mailer'); 
 
-var sendMessage = function(email, task){
+var sendMessage = function(data, task){
+
+    var html = '<ul>'; 
+    for(p in data.message){
+      html += '<ul>' + [p, data.message[p]].join(':') + '</ul>';  
+    }
+    html += '</ul>';  
 
     mailer({
-        email: email, 
+        email: data.meta._email, 
         subject: 'hello ', 
         text: 'hello world', 
-        html: '<h1>hello world</h2>' 
+        html: html,  
     }, function(err, response){
         if(err) return console.log('MAILER: ', err);  
         task.ack(); 
-        console.log('email is sent to ' + email + response.message);  
+        console.log('email is sent to ' + data.meta._email + response.message);  
     });    
 
 }
@@ -26,7 +32,23 @@ var parseMessage = function(str){
 		console.log(e); 
 	}
 
-	return data; 
+	if( ! data ) return data; 
+
+        var metaData = {}; 
+        var message  = {}; 
+
+        for(property in data){
+	  if(property[0] == '_'){
+	    metaData[property] = data[property]; 
+	  } else {
+            message[property] = data[property]; 
+	  }
+        } 
+        return {
+	  message: message, 
+	  meta: metaData
+        }; 
+
 }
 
 context.on('ready', function(){
@@ -35,8 +57,11 @@ context.on('ready', function(){
 		
 		sub.on('data', function(msg) {
 			console.log('RECIEVED: ', msg.toString());  
- 			var data = parseMessage(msg); 
-			sendMessage(data.email, sub); 
+ 			
+			var data = parseMessage(msg); 
+			console.log('PARSED: ', data); 
+			
+ 			sendMessage(data, sub); 
 		});
 	
 	}); 
